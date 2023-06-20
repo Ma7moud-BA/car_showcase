c909cb7cb4fecc3ce564c19bb39374e726ba833f
Ma7moud-BA <101318507+Ma7moud-BA@users.noreply.github.com>
2023-06-20 18:56:09 +0300

    update the hero section phrases

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,11 +8,10 @@ const Hero = () => {
 		<div className="hero">
 			<div className="flex-1 pt-36 padding-x">
 				<h1 className="hero__title">
-					Find, book, or rent a car -quickly and easily!
+					Discover the Perfect Ride - Effortlessly and Speedily!
 				</h1>
 				<p className="hero__subtitle">
-					Streamline your car rental experience with our effortless booking
-					process
+					Simplify Your Car Rental - Effortless Booking at Your Fingertips!
 				</p>
 				<CustomButton
 					title="Explore Cars"
